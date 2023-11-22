3d4853bbe0e1a269c46fb2093fd9081116e886cc
Serafim Prilezhaev <simkaa7@gmail.com>
2023-11-22 12:49:59 +0300

    AccessViewModel была создана

---
+++ Probotbor.View/Dialogs/AccessControl/AskDialog.cs
@@ -0,0 +1,20 @@
+﻿using Probotbor.Core.Contracts.Dialog;
+using System.Threading.Tasks;
+
+namespace Probotbor.View.Dialogs.AccessControl
+{
+    public class AskDialog : IQuestionDialog
+    {
+        public Task<bool> Ask(string title, string message)
+        {
+            AskDialogWindow window = new AskDialogWindow(); 
+            window.Title = title;
+            window.Content.Text = message;
+            if (window.ShowDialog() == true)
+            {
+                return Task.FromResult(true);
+            }
+            return Task.FromResult(false);
+        }
+    }
+}
---
+++ Probotbor.View/Dialogs/AccessControl/UserDialogWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Probotbor.Core.Models.AccessControl;
+using System.Windows;
+
+namespace Probotbor.View.Dialogs.AccessControl
+{
+    /// <summary>
+    /// Interaction logic for UserDialogWindow.xaml
+    /// </summary>
+    public partial class UserDialogWindow : Window
+    {
+        public UserDialogWindow()
+        {
+            InitializeComponent();
+        }
+
+        public UserDialogWindow(User user)
+        {
+            InitializeComponent();
+            this.DataContext = user;
+        }        
+
+        void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
